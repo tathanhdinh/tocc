692c240e41c32c79e05047cb73aa6d243f82bf20
Ta Thanh Dinh <tathanhdinh@gmail.com>
2019-10-23 02:10:56 +0200

    add tests

---
+++ README.md
@@ -0,0 +1,10 @@
+tocc: A type-obfuscated C compiler
+==================================
+
+
+### References
+ - [r9cc](https://github.com/utam0k/r9cc.git)
+ - [cproc](https://github.com/michaelforney/cproc)
+ - [lcc](https://github.com/drh/lcc)
+ - [Writing a C Compiler](https://norasandler.com/2017/11/29/Write-a-Compiler.html)
+ - [Small C Compilers](https://news.ycombinator.com/item?id=21210087)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{mem, slice};
 
 use cranelift::prelude::*;
+use cranelift_codegen::Context;
 use cranelift_module::{Linkage, Module};
 use cranelift_simplejit::{SimpleJITBackend, SimpleJITBuilder};
 
@@ -26,17 +27,65 @@ struct Opt {
 	expr: String,
 }
 
-fn compile(expr: &Expr, fb: &mut FunctionBuilder) -> Value {
+fn compile(
+	input: &str,
+	name: &str,
+	module: &mut Module<SimpleJITBackend>,
+	context: &mut Context,
+	function_builder_context: &mut FunctionBuilderContext,
+) -> (Box<fn() -> i64>, usize) {
+	// signature of main
+	context
+		.func
+		.signature
+		.returns
+		.push(AbiParam::new(types::I64));
+
+	// layout of main
+	// let mut func_builder_context = FunctionBuilderContext::new();
+	let mut func_builder =
+		FunctionBuilder::new(&mut context.func, function_builder_context);
+	let entry_block = func_builder.create_ebb();
+	func_builder.switch_to_block(entry_block);
+
+	// compile input arithmetic expression
+	let expr =
+		arithmetic::evaluate(&input).expect("Failed to parse input expression");
+	let expr_value = translate(&expr, &mut func_builder);
+	func_builder.ins().return_(&[expr_value]);
+
+	// finalize main
+	func_builder.seal_block(entry_block);
+	func_builder.finalize();
+
+	// push main into module
+	let func_id = module
+		.declare_function(name, Linkage::Export, &context.func.signature)
+		.expect("Failed to declare main function");
+	let func_len = module
+		.define_function(func_id, context)
+		.expect("Failed to define main function");
+
+	module.clear_context(context);
+	module.finalize_definitions(); // SELinux would not allow that, temporarily disable with "sudo setenforce 0"
+
+	let func_ptr = module.get_finalized_function(func_id);
+	let func_ptr = unsafe { mem::transmute::<_, _>(func_ptr) };
+
+	(Box::new(func_ptr), func_len as _)
+}
+
+fn translate(expr: &Expr, fb: &mut FunctionBuilder) -> Value {
 	match expr {
 		Expr::Add(lhs, rhs) => {
-			let lhs = compile(lhs, fb);
-			let rhs = compile(rhs, fb);
+			let lhs = translate(lhs, fb);
+			let rhs = translate(rhs, fb);
 			fb.ins().iadd(lhs, rhs)
 		}
 
 		Expr::Sub(lhs, rhs) => {
-			let lhs = compile(lhs, fb);
-			let rhs = compile(rhs, fb);
+			let lhs = translate(lhs, fb);
+			let rhs = translate(rhs, fb);
 			fb.ins().isub(lhs, rhs)
 		}
 
@@ -54,48 +103,19 @@ fn main() {
 		SimpleJITBuilder::new(cranelift_module::default_libcall_names());
 	let mut module = Module::<SimpleJITBackend>::new(builder);
 	let mut context = module.make_context();
-	let mut func_builder_context = FunctionBuilderContext::new();
+	let mut builder_context = FunctionBuilderContext::new();
 
-	// signature of main
-	context
-		.func
-		.signature
-		.returns
-		.push(AbiParam::new(types::I64));
-
-	// layout of main function
-	let mut func_builder =
-		FunctionBuilder::new(&mut context.func, &mut func_builder_context);
-	let entry_block = func_builder.create_ebb();
-	func_builder.switch_to_block(entry_block);
-
-	// compile input arithmetic expression
-	let expr = arithmetic::evaluate(&input_expr)
-		.expect("Failed to parse input expression");
-	let expr_value = compile(&expr, &mut func_builder);
-	func_builder.ins().return_(&[expr_value]);
-
-	// finalize main
-	func_builder.seal_block(entry_block);
-	func_builder.finalize();
-
-	// push main into module
-	let func_id = module
-		.declare_function("main", Linkage::Export, &context.func.signature)
-		.expect("Failed to declare main function");
-	let func_len = module
-		.define_function(func_id, &mut context)
-		.expect("Failed to define main function");
-
-	module.clear_context(&mut context);
-	// SELinux would not allow that, temporarily disable with "sudo setenforce 0"
-	module.finalize_definitions();
-
-	let func_ptr = module.get_finalized_function(func_id);
-	let jitted_func = unsafe { mem::transmute::<_, fn() -> i64>(func_ptr) };
+	let (func_ptr, func_len) = compile(
+		&input_expr,
+		"main",
+		&mut module,
+		&mut context,
+		&mut builder_context,
+	);
+	let func_ptr = *func_ptr;
 
 	// call jitted function
-	println!("result: {}", jitted_func());
+	println!("result: {}", func_ptr());
 
 	// print assembly code
 	let asm_formatter = {
@@ -108,7 +128,12 @@ fn main() {
 
 	let asm_decoder = Decoder::new(MachineMode::LONG_64, AddressWidth::_64)
 		.expect("Failed to create assembly decoder");
-	let func_code = unsafe { slice::from_raw_parts(func_ptr, func_len as _) };
+	let func_code = unsafe {
+		slice::from_raw_parts(
+			mem::transmute::<_, *const u8>(func_ptr),
+			func_len as _,
+		)
+	};
 
 	let mut decoded_inst_buffer = [0u8; 200];
 	let mut decoded_inst_buffer =
@@ -119,5 +144,37 @@ fn main() {
 			.format_instruction(&inst, &mut decoded_inst_buffer, Some(ip), None)
 			.expect("Failed to format instruction");
 		println!("0x{:x}\t{}", ip, decoded_inst_buffer);
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_expr() {
+		let builder =
+			SimpleJITBuilder::new(cranelift_module::default_libcall_names());
+		let mut module = Module::<SimpleJITBackend>::new(builder);
+		let mut context = module.make_context();
+		let mut builder_context = FunctionBuilderContext::new();
+
+		let (func_ptr, _) = compile(
+			"1 + 5 - 3",
+			"test0",
+			&mut module,
+			&mut context,
+			&mut builder_context,
+		);
+		assert_eq!(func_ptr(), 3);
+
+		let (func_ptr, _) = compile(
+			"2 - 3 - 7",
+			"test1",
+			&mut module,
+			&mut context,
+			&mut builder_context,
+		);
+		assert_eq!(func_ptr(), -8);
 	}
 }
