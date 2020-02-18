caf6bb567a6e0378f66f80cd77fac1123af5c724
Ta Thanh Dinh <tathanhdinh@gmail.com>
2020-02-18 02:03:38 +0100

    update

--- src/backend/function.rs
+++ src/backend/function.rs
@@ -170,64 +170,8 @@ fn blur_value(fb: &'_ mut FunctionBuilder, val: Value) -> Value {
 		let pv = ins!().ireduce(ty, pv);
 		let pv = match ty {
 			types::I8 => id!(i8, pv, ty, olevel),
-			// {
-			// 	if olevel == 0 {
-			// 		let (a0, b0, a1, b1) = generate_linear_maps!(i8);
-			// 		let pv = ins!().imul_imm(pv, a0 as i64);
-			// 		let pv = ins!().iadd_imm(pv, b0 as i64);
-			// 		let pv = ins!().imul_imm(pv, a1 as i64);
-			// 		let pv = ins!().iadd_imm(pv, b1 as i64);
-			// 		pv
-			// 	} else {
-			// 		let (coeffs, inv_coeffs) = generate_polynomial_maps!(i8, olevel);
-			// 		let x = pv;
-			// 		let mut px = ins!().iconst(ty, coeffs[0] as i64);
-			// 		for i in 1..=olevel {
-			// 			let mut x_pow_i = x;
-			// 			for _ in 0..i {
-			// 				x_pow_i = ins!().imul(x_pow_i, x);
-			// 			}
-			// 			x_pow_i = ins!().imul_imm(x_pow_i, coeffs[i as usize] as i64);
-			// 			px = ins!().iadd(px, x_pow_i);
-			// 		}
-
-			// 		let x = px;
-			// 		let mut qx = ins!().iconst(ty, inv_coeffs[0] as i64);
-			// 		for i in 1..=olevel {
-			// 			let mut x_pow_i = x;
-			// 			for _ in 0..i {
-			// 				x_pow_i = ins!().imul(x_pow_i, x);
-			// 			}
-			// 			x_pow_i = ins!().imul_imm(x_pow_i, inv_coeffs[i as usize] as i64);
-			// 			qx = ins!().iadd(px, x_pow_i);
-			// 		}
-
-			// 		qx
-			// 	}
-			// }
 			types::I16 => id!(i16, pv, ty, olevel),
-			// {
-			// 	if olevel == 0 {
-			// 		let (a0, b0, a1, b1) = generate_linear_maps!(i16);
-			// 		let pv = ins!().imul_imm(pv, a0 as i64);
-			// 		let pv = ins!().iadd_imm(pv, b0 as i64);
-			// 		let pv = ins!().imul_imm(pv, a1 as i64);
-			// 		let pv = ins!().iadd_imm(pv, b1 as i64);
-			// 		pv
-			// 	} else {
-			// 		todo!()
-			// 	}
-			// }
 			types::I32 => id!(i32, pv, ty, olevel),
-			// {
-			// 	let (a0, b0, a1, b1) = generate_linear_maps!(i32);
-			// 	let pv = fb.ins().imul_imm(pv, a0 as i64);
-			// 	let pv = fb.ins().iadd_imm(pv, b0 as i64);
-			// 	let pv = fb.ins().imul_imm(pv, a1 as i64);
-			// 	let pv = fb.ins().iadd_imm(pv, b1 as i64);
-			// 	pv
-			// }
-
 			_ => pv,
 		};
 
